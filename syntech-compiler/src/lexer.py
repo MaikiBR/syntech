cfbfff22c2356900b15d9ab34ad602919c85029f
Miguel Ángel Bermea Rodríguez <73300671+MaikiBR@users.noreply.github.com>
2024-05-08 21:28:11 -0600

    feat: syntech core grammar
    PENDING:
    - Test on web (syntech-web)
    - Update tests folder
    - Fix bugs when mixing expressions

--- syntech-compiler/src/lexer.py
+++ syntech-compiler/src/lexer.py
@@ -1,17 +1,49 @@
 import ply.lex as lex
 
 tokens = (
-    'HEADER',
-    'TEXT',
+    'HEADER1',
+    'HEADER2',
+    'HEADER3',
     'BOLD',
-    'ITALIC'
+    'ITALIC',
+    'BOLDITALIC',
+    'UNDERLINE',
+    'NEWLINE',
+    'ULISTSTART',
+    'ULISTEND',
+    'OLISTSTART',
+    'OLISTEND',
+    'LISTITEM',
+    'LINK',
+    'PLAINTEXT',
+    'CODE',
+    'IMAGE',
+    'ANYCHAR',
 )
 
-t_HEADER = r'\#\d\{.*?\}'
-t_TEXT = r'[^#*]+'
-t_BOLD = r'\*\*[^*]+\*\*'
-t_ITALIC = r'\*[^*]+\*'
+t_HEADER1 = r'\#1\{.*?\}'  # H101
+t_HEADER2 = r'\#2\{.*?\}'  # H102
+t_HEADER3 = r'\#3\{.*?\}'  # H103
+t_BOLD = r'\*\*[^*]+\*\*'  # H104
+t_ITALIC = r'\*[^*]+\*'  # H105
+t_BOLDITALIC = r'\*\*\*[^*]+\*\*\*'  # H106
+t_UNDERLINE = r'\_[^_]+\_'  # H107
+t_NEWLINE = r'\#'  # H108
+t_ULISTSTART = r'\\begin\{ulist\}'  # H109
+t_ULISTEND = r'\\end\{ulist\}'  # H109
+t_OLISTSTART = r'\\begin\{olist\}'  # H110
+t_OLISTEND = r'\\end\{olist\}'  # H110
+t_LISTITEM = r'\* .+'  # H109 y H110
+t_LINK = r'\[\[.*?\]\{.*?\}\]'  # H111
+t_PLAINTEXT = r'\".*?\"'  # H112
+t_CODE = r'\<\>.*?\<\>'  # H114
+t_IMAGE = r'\[\[\]img:.*?\]'  # H115
+t_ignore = ' \t'
 
+
+def t_ANYCHAR(t):
+    r'[^#*\_\\\<\>\[\]\{\}\"]+'
+    return t
 
 
 def t_error(t):
