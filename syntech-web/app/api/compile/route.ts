f47feca7c353c947a4ac37cde9769881cbe3af85
Miguel Ángel Bermea Rodríguez <73300671+MaikiBR@users.noreply.github.com>
2024-05-22 17:36:25 -0600

    fix: route.ts

--- syntech-web/app/api/compile/route.ts
+++ syntech-web/app/api/compile/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     const { text } = await request.json();
 
-    return new Promise((resolve) => {
+    return new Promise<void | Response>((resolve) => {
         const command = `python "C:/Users/mikeb/Documents/GitHub/syntech/syntech-compiler/src/main.py" "${text}"` // Change to global path
         exec(command, (error, stdout, stderr) => {
             if (error) {
