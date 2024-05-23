860eaacb5be6afc960179eb5a4bf0e5b4bfefc3b
Miguel Ángel Bermea Rodríguez <73300671+MaikiBR@users.noreply.github.com>
2024-05-22 18:59:23 -0600

    fixes

--- syntech-web/app/api/compile/route.ts
+++ syntech-web/app/api/compile/route.ts
@@ -1,12 +1,12 @@
-import { exec } from "child_process";
-import { NextResponse } from "next/server";
+const { exec } = require("child_process");
+const { NextResponse } = require("next/server");
 
 export async function POST(request: Request) {
     const { text } = await request.json();
 
-    return new Promise<void | Response>((resolve) => {
-        const command = `python api-python/main.py "${text}"` // Change to global path -> "C:/Users/mikeb/Documents/GitHub/syntech/syntech-compiler/src/main.py"
-        exec(command, (error, stdout, stderr) => {
+    return new Promise((resolve) => {
+        const command = `python api-python/main.py "${text}"`;
+        exec(command, (error: { message: any; }, stdout: any, stderr: any) => {
             if (error) {
                 console.error(`Error: ${error.message}`);
                 resolve(NextResponse.json({ error: error.message }, { status: 500 }));
