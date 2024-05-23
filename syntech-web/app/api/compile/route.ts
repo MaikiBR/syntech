fa2be0686178d0faea6b5ba03fd200cf14739c37
Miguel Ángel Bermea Rodríguez <73300671+MaikiBR@users.noreply.github.com>
2024-05-22 19:10:51 -0600

    testing new route.ts

--- syntech-web/app/api/compile/route.ts
+++ syntech-web/app/api/compile/route.ts
@@ -1,23 +1,26 @@
-import { exec } from "child_process";
-import { NextResponse } from "next/server";
+const { exec } = require("child_process");
+const { NextResponse } = require("next/server");
 
 export async function POST(request: Request): Promise<void | Response> {
-    const { text } = await request.json();
+  const { text } = await request.json();
 
-    return new Promise<void | Response>((resolve) => {
-        const command = `python api-python/main.py "${text}"`;
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                resolve(NextResponse.json({ error: error.message }, { status: 500 }));
-                return;
-            }
-            if (stderr) {
-                console.error(`Stderr: ${stderr}`);
-                resolve(NextResponse.json({ error: stderr }, { status: 500 }));
-                return;
-            }
-            resolve(NextResponse.json({ result: stdout }, { status: 200 }));
-        });
-    });
+  return new Promise<void | Response>((resolve) => {
+    const command = `python api-python/main.py "${text}"`;
+    exec(
+      command,
+      (error: { message: string } | null, stdout: string, stderr: string) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          resolve(NextResponse.json({ error: error.message }, { status: 500 }));
+          return;
+        }
+        if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+          resolve(NextResponse.json({ error: stderr }, { status: 500 }));
+          return;
+        }
+        resolve(NextResponse.json({ result: stdout }, { status: 200 }));
+      }
+    );
+  });
 }
