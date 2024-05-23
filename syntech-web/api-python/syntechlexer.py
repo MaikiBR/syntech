f41870129765151925c861fd44d10c71d2953c3b
Miguel Ángel Bermea Rodríguez <73300671+MaikiBR@users.noreply.github.com>
2024-05-22 21:02:19 -0600

    fix: Testing new regular expressions

--- syntech-web/api-python/syntechlexer.py
+++ syntech-web/api-python/syntechlexer.py
@@ -21,29 +21,25 @@ tokens = (
     'ANYCHAR',
 )
 
-t_HEADER1 = r'\#1\{.*?\}'  # H101
-t_HEADER2 = r'\#2\{.*?\}'  # H102
-t_HEADER3 = r'\#3\{.*?\}'  # H103
-t_BOLD = r'\*\*[^*]+\*\*'  # H104
-t_ITALIC = r'\*[^*]+\*'  # H105
-t_BOLDITALIC = r'\*\*\*[^*]+\*\*\*'  # H106
-t_UNDERLINE = r'\_[^_]+\_'  # H107
+t_HEADER1 = r'\#1\{[^\}]*\}'  # H101
+t_HEADER2 = r'\#2\{[^\}]*\}'  # H102
+t_HEADER3 = r'\#3\{[^\}]*\}'  # H103
+t_BOLD = r'\*\*[^\*]+\*\*'  # H104
+t_ITALIC = r'\*[^\*]+\*'  # H105
+t_BOLDITALIC = r'\*\*\*[^\*]+\*\*\*'  # H106
+t_UNDERLINE = r'\_[^\_]+\_'  # H107
 t_NEWLINE = r'\#'  # H108
 t_ULISTSTART = r'\\begin\{ulist\}'  # H109
 t_ULISTEND = r'\\end\{ulist\}'  # H109
 t_OLISTSTART = r'\\begin\{olist\}'  # H110
 t_OLISTEND = r'\\end\{olist\}'  # H110
-t_LISTITEM = r'\* .+'  # H109 y H110
-t_LINK = r'\[\[.*?\]\{.*?\}\]'  # H111
-t_PLAINTEXT = r'\".*?\"'  # H112
-t_CODE = r'\<\>.*?\<\>'  # H114
-t_IMAGE = r'\[\[\]img:.*?\]'  # H115
+t_LISTITEM = r'\* [^\n]+'  # H109 y H110
+t_LINK = r'\[\[[^\]]+\]\{[^\}]+\}\]'  # H111
+t_PLAINTEXT = r'\"[^\"]+\"'  # H112
+t_CODE = r'\<\>[^\<\>]+\<\>'  # H114
+t_IMAGE = r'\[\[\]img:[^\]]+\]'  # H115
+t_ANYCHAR = r'[^\#\*\_\\\<\>\[\]\{\}\"]+'  # Any character except special ones
 t_ignore = ' \t'
-
-
-def t_ANYCHAR(t):
-    r'[^#*\_\\\<\>\[\]\{\}\"]+'
-    return t
 
 
 def t_error(t):
--- syntech-web/api-python/syntechparser.py
+++ syntech-web/api-python/syntechparser.py
@@ -2,89 +2,61 @@ from syntechlexer import tokens
 import ply.yacc as yacc
 
 
-def p_expression_header1(p):
-    'expression : HEADER1'
-    p[0] = f'<h1>{p[1][3:-1]}</h1>'
-
-
-def p_expression_header2(p):
-    'expression : HEADER2'
-    p[0] = f'<h2>{p[1][3:-1]}</h2>'
-
-
-def p_expression_header3(p):
-    'expression : HEADER3'
-    p[0] = f'<h3>{p[1][3:-1]}</h3>'
-
-
-def p_expression_bold(p):
-    'expression : BOLD'
-    p[0] = f'<b>{p[1][2:-2]}</b>'
-
-
-def p_expression_italic(p):
-    'expression : ITALIC'
-    p[0] = f'<i>{p[1][1:-1]}</i>'
-
-
-def p_expression_bolditalic(p):
-    'expression : BOLDITALIC'
-    p[0] = f'<b><i>{p[1][3:-3]}</i></b>'
-
-
-def p_expression_underline(p):
-    'expression : UNDERLINE'
-    p[0] = f'<u>{p[1][1:-1]}</u>'
-
-
-def p_expression_newline(p):
-    'expression : NEWLINE'
-    p[0] = '<br>'
-
-
-def p_expression_ulist(p):
-    'expression : ULISTSTART expression ULISTEND'
-    p[0] = f'<ul>{p[2]}</ul>'
-
-
-def p_expression_olist(p):
-    'expression : OLISTSTART expression OLISTEND'
-    p[0] = f'<ol>{p[2]}</ol>'
-
-
-def p_expression_listitem(p):
-    'expression : LISTITEM'
-    p[0] = f'<li>{p[1][2:]}</li>'
-
-
-def p_expression_link(p):
-    'expression : LINK'
-    url, title = p[1][2:-1].split(']{')
-    p[0] = f'<a href="{url}">{title}</a>'
-
-
-def p_expression_plaintext(p):
-    'expression : PLAINTEXT'
-    p[0] = p[1][1:-1]
-
-
-def p_expression_code(p):
-    'expression : CODE'
-    p[0] = f'<code>{p[1][2:-2]}</code>'
-
-
-def p_expression_image(p):
-    'expression : IMAGE'
-    p[0] = f'<img src="{p[1][7:-1]}" />'
-
-
-def p_expression_anychar(p):
-    '''expression : ANYCHAR
-                  | expression ANYCHAR'''
-    if len(p) == 2:
-        p[0] = p[1]
-    else:
+def p_expression(p):
+    '''expression : expression expression
+                  | HEADER1
+                  | HEADER2
+                  | HEADER3
+                  | BOLD
+                  | ITALIC
+                  | BOLDITALIC
+                  | UNDERLINE
+                  | NEWLINE
+                  | ULISTSTART expression ULISTEND
+                  | OLISTSTART expression OLISTEND
+                  | LISTITEM
+                  | LINK
+                  | PLAINTEXT
+                  | CODE
+                  | IMAGE
+                  | ANYCHAR'''
+    if len(p) == 3:
         p[0] = p[1] + p[2]
+    elif len(p) == 4:
+        if p[1] == '\\begin{ulist}' and p[3] == '\\end{ulist}':
+            p[0] = f'<ul>{p[2]}</ul>'
+        elif p[1] == '\\begin{olist}' and p[3] == '\\end{olist}':
+            p[0] = f'<ol>{p[2]}</ol>'
+    else:
+        if p.slice[1].type == 'HEADER1':
+            p[0] = f'<h1>{p[1][3:-1]}</h1>'
+        elif p.slice[1].type == 'HEADER2':
+            p[0] = f'<h2>{p[1][3:-1]}</h2>'
+        elif p.slice[1].type == 'HEADER3':
+            p[0] = f'<h3>{p[1][3:-1]}</h3>'
+        elif p.slice[1].type == 'BOLD':
+            p[0] = f'<b>{p[1][2:-2]}</b>'
+        elif p.slice[1].type == 'ITALIC':
+            p[0] = f'<i>{p[1][1:-1]}</i>'
+        elif p.slice[1].type == 'BOLDITALIC':
+            p[0] = f'<b><i>{p[1][3:-3]}</i></b>'
+        elif p.slice[1].type == 'UNDERLINE':
+            p[0] = f'<u>{p[1][1:-1]}</u>'
+        elif p.slice[1].type == 'NEWLINE':
+            p[0] = '<br>'
+        elif p.slice[1].type == 'LISTITEM':
+            p[0] = f'<li>{p[1][2:]}</li>'
+        elif p.slice[1].type == 'LINK':
+            url, title = p[1][2:-1].split(']{')
+            p[0] = f'<a href="{url}">{title}</a>'
+        elif p.slice[1].type == 'PLAINTEXT':
+            p[0] = p[1][1:-1]
+        elif p.slice[1].type == 'CODE':
+            p[0] = f'<code>{p[1][2:-2]}</code>'
+        elif p.slice[1].type == 'IMAGE':
+            p[0] = f'<img src="{p[1][7:-1]}" />'
+        elif p.slice[1].type == 'ANYCHAR':
+            p[0] = p[1]
 
 
 def p_error(p):
